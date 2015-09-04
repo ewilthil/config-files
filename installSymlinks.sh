67e69d8247bdbf98197a9a9ec7d5f9d7c4b0bb2e
Erik Wilthil <ewilthil@dhcp-020033.wlan.ntnu.no>
2015-09-04 18:31:03 +0200

    Added a couple useful bash scripts

---
+++ installSymlinks.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+dir=~/config-files
+olddir=~/conf_old
+files=".vimrc .gitconfig .aliases .bashrc .bashfunctions.sh"
+
+
+echo -n "Creating $olddir for backup of any existing dotfiles in ~ ..."
+mkdir -p $olddir
+echo "Done"
+
+echo -n "Changing to the $dir directory ..."
+cd $dir
+echo "Done"
+
+for file in $files; do
+	mv ~/$file ~/olddir/
+	ln -s $dir/$file ~/$file
+done
